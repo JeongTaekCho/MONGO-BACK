34656e404f7beade4cf94c8c2b984021653e7727
JeongTaekCho <cjt3591@gmail.com>
2023-02-16 20:07:24 +0900

    ✨ Feat :  백엔드 생성

---
+++ mongo.js
@@ -0,0 +1,11 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url =
+  "mongodb+srv://cjt3591:x156156a!@cluster0.gscm3cy.mongodb.net/places_test?retryWrites=true&w=majority";
+
+const createProduct = async (req, res, next) => {};
+
+const getProducts = async (req, res, next) => {};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
