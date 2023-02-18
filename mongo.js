4c0f02e9c55846ada3ed3a54f6db20dbfe5773c8
JeongTaekCho <cjt3591@gmail.com>
2023-02-18 19:20:36 +0900

    ✨ Feat :  createProduct 몽고DB 업로드

--- mongo.js
+++ mongo.js
@@ -1,9 +1,27 @@
 const MongoClient = require("mongodb").MongoClient;
 
 const url =
-  "mongodb+srv://cjt3591:x156156a!@cluster0.gscm3cy.mongodb.net/places_test?retryWrites=true&w=majority";
+  "mongodb+srv://cjt3591:xc156156a!@cluster0.gscm3cy.mongodb.net/products_test?retryWrites=true&w=majority";
 
-const createProduct = async (req, res, next) => {};
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = await db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "Could not store data" });
+  }
+  client.close();
+
+  res.json(newProduct);
+};
 
 const getProducts = async (req, res, next) => {};
 
